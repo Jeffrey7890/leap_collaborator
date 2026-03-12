49419b5eada89e274b0820fae7c65c8b67bbba7c
Ogwu Jeffrey <55333325+Jeffrey7890@users.noreply.github.com>
2026-03-13 00:06:56 +0100

    new changes

--- leap_abstraction.py
+++ leap_abstraction.py
@@ -1,4 +1,33 @@
 import win32com.client
+import json
+from enum import Enum
+
+class OBJType(Enum):
+	BRANCH = 'Branch'
+	SCENARIO = 'Scenario'
+
+
+
+class LEAPObject:
+	def __init__(self, COMObject, ObjType):
+		self.object = COMObject
+		self.type = ObjType
+
+	def __repr__(self):
+		return f"<LEAPObject <{self.type}: {self.object.Name}>>"
+	
+	def name(self):
+		return self.object.Name
+
+	def get_variables(self, BaseYear=2020, EndYear=2040):
+		result = {}
+		if self.type in self.object.BranchTypeName:
+			variables = self.object.Variables
+			for v in variables:
+				value_by_years = [v.Value(year) for year in range(BaseYear, EndYear+10, 10)]
+				result[v.Name] = (value_by_years, v.DefaultResultUnit.Name)
+		return result
+
 
 
 class LeapModel():
@@ -7,14 +36,43 @@ class LeapModel():
 		if not self.leap:
 			raise("Leap not open")
 
-	def get_branch_results(self, branch):
-		result = {}
-		variables = self.leap.Branch(branch).Variables
-		for v in variables:
-			result[v.BranchVariableName] = v.Value()
+	def __repr__(self):
+		return "<<LEAPObject>>"
+
+	def get_branch(self, branch_path):
+		branch = self.leap.Branch(branch_path)
+		return LEAPObject(branch, "Branch")
+
+	def get_current_scenario_results(self, branch_path="Demand"):
+		branch = self.get_branch(branch_path)
+		self.leap.Calculate()
+		branch_result = branch.get_variables(self.leap.BaseYear, self.leap.EndYear)
+		return {"Scenario":self.leap.ActiveScenario.Name, branch_path:branch_result}
+
+	def get_scenario_results(self, scenario, branch_path):
+		self.leap.ActiveScenario = scenario
+		return self.get_current_scenario_results(branch_path)
+	
+	def get_multiple_scenario_results(self, scenarios:list, branch_paths='Demand'):
+		"""
+			get_multiple_scenario_results: get data for the given list of scenarios
+			@scenarios: list of scenarios
+			@branch_paths: should be list [leave as Demand for now
+			returns: a list of all scenario data
+		"""
+		result = []
+		for scenario in scenarios:
+			result.append(self.get_scenario_results(scenario, branch_paths))
 		return result
 
-if __name__== "__main__":
-	L = LeapModel()
-	print(L.get_branch_results("Demand"))
+
+
+def save_json(data):
+	with open("scenario_datas.json", 'w')as f:
+		json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+	leap = LeapModel()
+	SCD = leap.get_multiple_scenario_results(scenarios = ['Baseline', 'Mitigation'])
+	save_json(SCD)
 
