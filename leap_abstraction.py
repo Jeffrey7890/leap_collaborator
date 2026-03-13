eea8dcc3f9cf571305c4d0c7f2feff2af1b1b398
Ogwu Jeffrey <55333325+Jeffrey7890@users.noreply.github.com>
2026-03-13 15:25:07 +0100

    ui implemented, to look at scenarios

--- leap_abstraction.py
+++ leap_abstraction.py
@@ -21,11 +21,19 @@ class LEAPObject:
 
 	def get_variables(self, BaseYear=2020, EndYear=2040):
 		result = {}
-		if self.type in self.object.BranchTypeName:
-			variables = self.object.Variables
-			for v in variables:
-				value_by_years = [v.Value(year) for year in range(BaseYear, EndYear+10, 10)]
-				result[v.Name] = (value_by_years, v.DefaultResultUnit.Name)
+		variables = self.object.Variables
+		for v in variables:
+			value_by_years = [v.Value(year) for year in range(BaseYear, EndYear+10, 10)]
+			units = v.DefaultResultUnit
+			if units is not None:
+				result[v.Name] = (value_by_years, units.Name)
+			else:
+				units = v.DataUnitText
+				result[v.Name] = (value_by_years, units)
+			# try:
+			# 	result[v.Name] = (value_by_years, v.DefaultResultUnit.Name)
+			# except:
+			# 	result[v.Name] = (value_by_years, v.DataUnitText)
 		return result
 
 
@@ -43,11 +51,25 @@ class LeapModel():
 		branch = self.leap.Branch(branch_path)
 		return LEAPObject(branch, "Branch")
 
+	def get_subbranch(self, branch_path):
+		branch = self.leap.Branch(branch_path)
+		branch_list = []
+		def crawl(branch):
+			for b in branch.Children:
+				branch_list.append(b.FullName)
+				crawl(b)
+		crawl(branch);
+		return branch_list
+
+
+
 	def get_current_scenario_results(self, branch_path="Demand"):
 		branch = self.get_branch(branch_path)
-		self.leap.Calculate()
-		branch_result = branch.get_variables(self.leap.BaseYear, self.leap.EndYear)
-		return {"Scenario":self.leap.ActiveScenario.Name, branch_path:branch_result}
+		BaseYear = self.leap.BaseYear
+		EndYear = self.leap.EndYear
+		branch_result = branch.get_variables(BaseYear, EndYear)
+		return {"years":[year for year in range(BaseYear, EndYear+10, 10)],
+				"Scenario":self.leap.ActiveScenario.Name, branch_path:branch_result}
 
 	def get_scenario_results(self, scenario, branch_path):
 		self.leap.ActiveScenario = scenario
@@ -61,18 +83,24 @@ class LeapModel():
 			returns: a list of all scenario data
 		"""
 		result = []
+		self.leap.Calculate()
 		for scenario in scenarios:
 			result.append(self.get_scenario_results(scenario, branch_paths))
 		return result
 
 
 
-def save_json(data):
-	with open("scenario_datas.json", 'w')as f:
+def save_json(data, name):
+	with open(name+ ".json", 'w')as f:
 		json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
 	leap = LeapModel()
+	sub_branches = leap.get_subbranch("Transformation")
 	SCD = leap.get_multiple_scenario_results(scenarios = ['Baseline', 'Mitigation'])
-	save_json(SCD)
+	save_json(SCD, 'Transformation')
+
+	# lighting_existing = leap.get_branch("Demand\\Household\\Urban\\Electrified\\Lighting\\Existing")
+	# var = lighting_existing.get_variables()
+	# print(var)
 
